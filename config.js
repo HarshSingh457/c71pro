32fa5e19a703ce016e8e3236ed341bb5962d3333
Harsh Singh <77051378+HarshSingh457@users.noreply.github.com>
2022-08-19 00:27:15 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyB1isphGUF89hWRImax3iQLOLkLfb-NEZ4",
+  authDomain: "c71pro-8e6f0.firebaseapp.com",
+  projectId: "c71pro-8e6f0",
+  storageBucket: "c71pro-8e6f0.appspot.com",
+  messagingSenderId: "947720020574",
+  appId: "1:947720020574:web:8a036642b2e59bf77f8acf"
+};
 //
 firebase.initializeApp(firebaseConfig);
 
